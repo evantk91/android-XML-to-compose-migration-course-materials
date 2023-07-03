8abc95c613057cce02bc79ff03d02247722d519e
evan.greer@iterable.com <evan.greer@evan.greer>
2023-07-03 14:50:38 -0600

    updates animal detail fragment with composable for text

--- course-starter-project/app/src/main/java/com/catalin/mvvmanimalslist/view/AnimalDetailFragment.kt
+++ course-starter-project/app/src/main/java/com/catalin/mvvmanimalslist/view/AnimalDetailFragment.kt
@@ -4,6 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.catalin.mvvmanimalslist.R
@@ -33,10 +41,13 @@ class AnimalDetailFragment : Fragment() {
                     Glide.with(detailImage.context)
                         .load(url)
                         .into(detailImage)
-                    detailTitle.text = it.name
-                    detailLocation.text = getString(R.string.detail_location, it.location)
-                    detailLifespan.text = getString(R.string.detail_lifespan, it.lifespan)
-                    detailDiet.text = getString(R.string.detail_diet, it.diet)
+                    composeView.setContent {
+                        AnimalDetails(animal = it)
+                    }
+//                    detailTitle.text = it.name
+//                    detailLocation.text = getString(R.string.detail_location, it.location)
+//                    detailLifespan.text = getString(R.string.detail_lifespan, it.lifespan)
+//                    detailDiet.text = getString(R.string.detail_diet, it.diet)
                 }
             }
 
@@ -45,6 +56,19 @@ class AnimalDetailFragment : Fragment() {
 
     companion object {
         const val currentAnimal = "currentAnimal"
+    }
+}
+
+@Composable
+fun AnimalDetails(animal: Animal) {
+    val titleSize = dimensionResource(id = R.dimen.std_title).value.sp
+    val textSize = dimensionResource(id = R.dimen.std_text).value.sp
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = animal.name ?: "", fontSize = titleSize, fontWeight = FontWeight.Bold)
+        Text(text = stringResource(id = R.string.detail_location, animal.location ?: ""), fontSize = textSize)
+        Text(text = stringResource(id = R.string.detail_lifespan, animal.lifespan ?: ""), fontSize = textSize)
+        Text(text = stringResource(id = R.string.detail_diet, animal.diet ?: ""), fontSize = textSize)
     }
 }
 

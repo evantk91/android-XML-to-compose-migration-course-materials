f285c90a9e43caac2d00de7406301f659cc39d12
evan.greer@iterable.com <evan.greer@evan.greer>
2023-07-04 11:39:42 -0600

    converts main activity to composable based

--- course-starter-project/app/src/main/java/com/catalin/mvvmanimalslist/view/MainActivity.kt
+++ course-starter-project/app/src/main/java/com/catalin/mvvmanimalslist/view/MainActivity.kt
@@ -1,16 +1,44 @@
 package com.catalin.mvvmanimalslist.view
 
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidViewBinding
 import com.catalin.mvvmanimalslist.R
+import com.catalin.mvvmanimalslist.databinding.ActivityMainBinding
+import com.catalin.mvvmanimalslist.ui.theme.AnimalListTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            AnimalListTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    AnimalListFragmentComposable()
+                }
+            }
+        }
     }
+}
+
+@Composable
+fun AnimalListFragmentComposable() {
+    AndroidViewBinding(ActivityMainBinding::inflate)
 }
